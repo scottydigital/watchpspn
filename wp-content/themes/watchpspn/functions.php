ad1f09f5ac2890c5cfe69674205813a0f018de99
Scott Walker <scott@scottydigital.com>
2026-08-28 15:54:39 -0400

    Improve responsive copyright and CSS cache busting

--- wp-content/themes/watchpspn/functions.php
+++ wp-content/themes/watchpspn/functions.php
@@ -1,12 +1,14 @@
 <?php
 
 function watchpspn_enqueue_styles() {
+    $style_path = get_stylesheet_directory() . '/style.css';
+
     wp_enqueue_style(
         'watchpspn-style',
         get_stylesheet_uri(),
         array(),
-        wp_get_theme()->get('Version')
+        filemtime( $style_path )
     );
 }
 
-add_action('wp_enqueue_scripts', 'watchpspn_enqueue_styles');
+add_action( 'wp_enqueue_scripts', 'watchpspn_enqueue_styles' );
